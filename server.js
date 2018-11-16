ef2558174dd58e589389e6f39cc417eee5254dd2
Ivana Lee <lee.ivana@hotmail.com>
2018-11-16 16:27:27 +0000

    Added cookie-session as a dependency

--- server.js
+++ server.js
@@ -2,17 +2,18 @@
 
 require('dotenv').config();
 
-const PORT        = process.env.PORT || 8080;
-const ENV         = process.env.ENV || "development";
-const express     = require("express");
-const bodyParser  = require("body-parser");
-const sass        = require("node-sass-middleware");
-const app         = express();
+const PORT              = process.env.PORT || 8080;
+const ENV               = process.env.ENV || "development";
+const express           = require("express");
+const bodyParser        = require("body-parser");
+const sass              = require("node-sass-middleware");
+const cookieSession     = required("cookie-session");
+const app               = express();
 
-const knexConfig  = require("./knexfile");
-const knex        = require("knex")(knexConfig[ENV]);
-const morgan      = require('morgan');
-const knexLogger  = require('knex-logger');
+const knexConfig        = require("./knexfile");
+const knex              = require("knex")(knexConfig[ENV]);
+const morgan            = require('morgan');
+const knexLogger        = require('knex-logger');
 
 // Seperated Routes for each Resource
 const menuRoutes = require("./routes/menu");
@@ -26,6 +27,12 @@ app.use(morgan('dev'));
 
 // Log knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
+
+//To set cookies
+app.use(cookieSession({
+  name: 'session',
+  secret: 'keypass'
+}));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
