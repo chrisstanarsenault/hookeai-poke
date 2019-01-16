aeafa77de64ba5cf9e6d7ed115501b5e189a42e5
Ryan Kendrick <rpkendrick@gmail.com>
2019-01-16 15:35:59 +0000

    heroku testing

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.get("/", (req, res) => {
   res.render("main", templateVars);
 });
 
-HTTP.createServer(app).listen(PORT, () => {
+HTTP.createServer(app).listen(8080, () => {
   console.log('Express server listening on port 8080');
 });
 
